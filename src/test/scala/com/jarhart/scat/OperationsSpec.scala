1b9a47d7492be0803b79ce732a7f13c17c652808
Andrew Mohrland <amohrland@gmail.com>
2014-02-08 18:57:09 -0800

    Add boolean operations

--- src/test/scala/com/jarhart/scat/OperationsSpec.scala
+++ src/test/scala/com/jarhart/scat/OperationsSpec.scala
@@ -55,7 +55,6 @@ class OperationsSpec extends UnitSpec {
     }
   }
 
-
   "div divides the top value on the stack by the one below" in {
     forAll { (x: Float, y: Float, stack: HList) =>
       whenever(y != 0.0) {
@@ -63,6 +62,30 @@ class OperationsSpec extends UnitSpec {
           div.run(y :: x :: stack)._2 === (x / y) :: stack
         )
       }
+    }
+  }
+
+  "and logically ANDs the top two values on the stack" in {
+    forAll { (x: Boolean, y: Boolean, stack: HList) =>
+      assert(
+        and.run(y :: x :: stack)._2 === (x && y) :: stack
+      )
+    }
+  }
+
+  "or logically ORs the top two values on the stack" in {
+    forAll { (x: Boolean, y: Boolean, stack: HList) =>
+      assert(
+        or.run(y :: x :: stack)._2 === (x || y) :: stack
+      )
+    }
+  }
+
+  "not logically NOTs the top value on the stack" in {
+    forAll { (x: Boolean, stack: HList) =>
+      assert(
+        not.run(x :: stack)._2 === (!x) :: stack
+      )
     }
   }
 }
